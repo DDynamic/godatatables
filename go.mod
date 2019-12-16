ebd40fbd11a45720aa4d4ffb9e228ea1b2dbd3af
Dylan Seidt <dylan.seidt@gmail.com>
2019-12-15 22:18:49 -0600

    Push test files

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/ddynamic/godatatables
 go 1.13
 
 require (
+	github.com/jinzhu/gorm v1.9.11
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/joho/godotenv v1.3.0
 	google.golang.org/appengine v1.6.5 // indirect
 )
---
+++ test/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+
+	"github.com/ddynamic/godatatables"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	env, _ := godotenv.Read()
+
+	if env["DATABASE_URL"] == "" {
+		env["DATABASE_URL"] = "root:password@tcp(127.0.0.1:3306)/test?parseTime=true&charset=utf8mb4,utf8"
+	}
+
+	godotenv.Write(env, "./.env")
+
+	godotenv.Load()
+
+	tmpl := template.Must(template.ParseFiles("test.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, nil)
+	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		db, err := sql.Open("mysql", os.Getenv("DATABASE_URL"))
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		godatatables.DataTables(db, "members", "id, name", w, r)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
